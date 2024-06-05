fdfa46f47c3afb927b9c9687702478da920eddb8
JOAO PEDRO BRAGA VEIGA <119889222+JoaoPedroVeiga@users.noreply.github.com>
2024-06-05 12:36:38 -0300

    configuração do build da aplicação

--- subtracao.test.ts
+++ subtracao.test.ts
@@ -1,13 +1,13 @@
 import subtracao from "./subtracao"; 
 
 describe('Testes de subtração', () => {
-    test('deve verificar o resultado de uma sbtracao', () => {
+    test('deve verificar o resultado de uma subtração', () => {
         expect(subtracao(8.7, 3.5)).toBeCloseTo(5.2) // toBeCloseTo cerve para ponto flutuante 
         expect(subtracao(8,2)).toBeCloseTo(6)
         expect(subtracao(10,3)).toBeCloseTo(7)
     })
 
-    test('deve verificar o resultado incorreto de uma sbtracao', () => {
+    test('deve verificar o resultado incorreto de uma subtração', () => {
         expect(subtracao(8,3)).not.toBeCloseTo(4)
     })
 })
