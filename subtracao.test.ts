b608cd160a2d66376c1142e00ae9c998ad285b3c
JOAO PEDRO BRAGA VEIGA <119889222+JoaoPedroVeiga@users.noreply.github.com>
2024-06-05 11:31:11 -0300

    implementação da função sbtração

---
+++ subtracao.test.ts
@@ -0,0 +1,14 @@
+import subtracao from "./subtracao"; 
+
+describe('Testes de subtração', () => {
+    test('deve verificar o resultado de uma sbtracao', () => {
+        expect(subtracao(8.7, 3.5)).toBeCloseTo(5.2) // toBeCloseTo cerve para ponto flutuante 
+        expect(subtracao(8,2)).toBeCloseTo(6)
+        expect(subtracao(10,3)).toBeCloseTo(7)
+    })
+
+    test('deve verificar o resultado incorreto de uma sbtracao', () => {
+        expect(subtracao(8,3)).not.toBeCloseTo(4)
+    })
+})
+
